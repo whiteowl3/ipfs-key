85906c8681e4265243fc24b98d399170ee12f198
whiteowl3 <71030468+whiteowl3@users.noreply.github.com>
2022-04-09 03:05:01 -0400

    Update main.go

--- main.go
+++ main.go
@@ -1,15 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
 
 	b64 "encoding/base64"
+
 	ci "github.com/libp2p/go-libp2p-crypto"
 	peer "github.com/libp2p/go-libp2p-peer"
 )
+
+type Identity struct {
+	ID      string `json:"ID"`
+	PrivKey string `json:"PrivKey"`
+}
 
 func main() {
 	size := flag.Int("bitsize", 2048, "select the bitsize of the key to generate")
@@ -29,27 +36,39 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Fprintf(os.Stderr, "Generating a %d bit %s key...\n", *size, *typ)
-	priv, pub, err := ci.GenerateKeyPair(atyp, *size)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	//m := make(map[string]Identity)
+	m := make(map[string]string)
+	for i := 0; i < *quant; i++ {
+		//fmt.Fprintf(os.Stderr, "Generating a %d bit %s key...\n", *size, *typ)
+		priv, pub, err := ci.GenerateKeyPair(atyp, *size)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		//fmt.Fprintln(os.Stderr, "Success!")
+
+		pid, err := peer.IDFromPublicKey(pub)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		//fmt.Println(pid.Pretty())
+
+		data, err := priv.Bytes()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+
+		//m[fmt.Sprint(i)] = Identity{
+		//	ID:      pid.Pretty(),
+		//	PrivKey: b64.StdEncoding.EncodeToString(data),
+		//}
+
+		m[pid.Pretty()] = b64.StdEncoding.EncodeToString(data)
+
 	}
-	fmt.Fprintln(os.Stderr, "Success!")
-
-	pid, err := peer.IDFromPublicKey(pub)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	fmt.Fprintf(os.Stderr, "ID for generated key: %s\n", pid.Pretty())
-
-	data, err := priv.Bytes()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
-
-	os.Stdout.Write(b64.StdEncoding.EncodeToString(data))
+	jsonStr, _ := json.Marshal(m)
+	fmt.Print(string(jsonStr))
 }
